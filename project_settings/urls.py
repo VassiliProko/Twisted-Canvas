46f0189358cbebaf2eb6ccfb2217e07eca98e869
VassiliProko <vassiligb12@gmail.com>
2025-07-20 11:09:02 -0400

    change urls.py structure

--- project_settings/urls.py
+++ project_settings/urls.py
@@ -16,19 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
-from main_app import views
+from django.urls import path, include
 
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("create/", views.create_art, name="create_art"),
-    path("canvas/<int:art_id>/", views.canvas_view, name="canvas_view"),
-    path("save-art/", views.save_art, name="save_art"),
-    path("rename-art/<int:art_id>/", views.rename_art, name="rename_art"),
-    path("delete-art/<int:art_id>/", views.delete_art, name="delete_art"),
-    path("export-art/<int:art_id>/", views.export_art, name="export_art"),
-    path("login/", views.login_view, name="login"),
-    path("register/", views.register, name="register"),
-    path("logout/", views.logout_view, name="logout"),
-    path("admin/", admin.site.urls),
+    path("admin/", include("main_app.urls")),
+    path("", include("main_app.urls"))
 ]
